e58856ef63e2063496792e98ea5529e44a0e01ac
Hamza Hasan <hamzahasanellahie000@gmail.com>
2023-02-24 15:31:02 +0500

    add fastgcd

---
+++ content/number-theory/gcdFast.h
@@ -0,0 +1,27 @@
+/**
+ * Author: H2
+ * Date: 2023-02-24
+ * License: CC0
+ * Source: CF
+ * Description: Faster gcd
+ * Time: O(\log N)
+ * Status: Stress-tested
+ */
+#pragma once
+
+int gcd(int u, int v) { // faster
+    int shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do{
+        int m;
+        v >>= __builtin_ctz(v);
+        v -= u;
+        m = v >> 31;
+        u += v & m;
+        v = (v + m) ^ m;
+    } while (v != 0);
+    return u << shift;
+}
